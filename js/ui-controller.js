59e6c858eaf89fa868458bd9edfada20bafa0c6e
nicolasgodoy <nicolasgodoy540@gmail.com>
2026-01-26 23:29:07 -0300

    subiendo cambios de pagos

--- js/ui-controller.js
+++ js/ui-controller.js
@@ -144,7 +144,7 @@ class UIController {
         if (result.success) {
             this.loginModal.style.display = 'none';
             this.mainApp.style.display = 'block';
-            this.currentUserDisplay.innerHTML = `<svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" style="margin-right: 5px;"><path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"/><circle cx="12" cy="7" r="4"/></svg> ${username}`;
+            this.currentUserDisplay.innerHTML = `<svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" style="margin-right: 5px;"><path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"/><circle cx="12" cy="7" r="4"/></svg> <span class="hide-mobile">${username}</span>`;
             this.userName.textContent = username;
 
             // Set initial
